75d507d823296f7db5f92fdff0f2a5e7bbc08410
Limai <sasha.nafasov@gmail.com>
2022-09-24 23:20:25 +0500

    добавлен гендер

--- src/types/VK/Responses/GetUserResponse.ts
+++ src/types/VK/Responses/GetUserResponse.ts
@@ -1,13 +1,14 @@
 export type GetUserResponse = {
-  bdate: string;
-  can_access_closed: boolean;
+  bdate: string
+  can_access_closed: boolean
   city: {
-    id: number;
-    title: string;
-  };
-  first_name: string;
-  id: number;
-  is_closed: boolean;
-  last_name: string;
-  screen_name: string;
+    id: number
+    title: string
+  }
+  first_name: string
+  id: number
+  is_closed: boolean
+  last_name: string
+  screen_name: string
+  sex: number
 };
