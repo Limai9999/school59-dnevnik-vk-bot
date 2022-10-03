e0ca135cd026a602264ce8d561d350a708b779ab
Limai <sasha.nafasov@gmail.com>
2022-10-04 00:04:57 +0500

    заготовка команды оценка

---
+++ src/commands/grades.ts
@@ -0,0 +1,33 @@
+import {CommandInputData, CommandOutputData} from '../types/Commands';
+
+import {GradesPayload} from '../types/VK/Payloads/GradesPayload';
+
+async function command({message, classes, args, vk}: CommandInputData) {
+  await classes.setLoading(message.peerId, false);
+
+  await vk.sendMessage({
+    peerId: message.peerId,
+    message: 'Эта команда еще не реализована.',
+  });
+};
+
+const cmd: CommandOutputData = {
+  name: 'оценки',
+  aliases: ['grades'],
+  description: null,
+  payload: {
+    command: 'grades',
+    data: {action: 'today'},
+  } as GradesPayload,
+  requirements: {
+    admin: false,
+    dmOnly: false,
+    args: 0,
+  },
+  showInAdditionalMenu: false,
+  showInCommandsList: true,
+  howToUse: null,
+  execute: command,
+};
+
+export default cmd;
