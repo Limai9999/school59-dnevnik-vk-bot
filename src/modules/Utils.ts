9e47b1cda488e9b03fd8ab0d96f1507b7559b26c
Limai <sasha.nafasov@gmail.com>
2022-10-09 02:07:17 +0500

    форматирование окончание слова (будет переделано)

--- src/modules/Utils.ts
+++ src/modules/Utils.ts
@@ -1,6 +1,18 @@
 import {GetCookiesResponse} from '../types/Responses/API/netCity/GetCookiesResponse';
+import {Declination, WordGender} from '../types/Utils/SetWordEnding';
 
 export default class Utils {
+  types = {
+    Declination,
+    WordGender,
+  };
+
+  constructor() {
+    this.types = {
+      Declination,
+      WordGender,
+    };
+  }
   genderifyWord(word: string, sex: number) {
     switch (sex) {
       case 0:
@@ -24,5 +36,19 @@ export default class Utils {
 
   checkIfPeerIsDM(peerId: number) {
     return peerId < 2000000000;
+  }
+
+  setWordEndingBasedOnThingsCount(unformattedWord: string, declination: Declination, wordGender: WordGender, things: number): string {
+    const thingsStr = String(things);
+
+    if (declination === Declination.Second) {
+      if (wordGender === WordGender.Masculine) {
+        if (thingsStr.endsWith('0') || (things >= 10 && !thingsStr.endsWith('1') && !thingsStr.endsWith('2'))) return unformattedWord + 'ов';
+        if (thingsStr.endsWith('1')) return unformattedWord;
+        if (thingsStr.endsWith('2')) return unformattedWord + 'а';
+      }
+    }
+
+    return unformattedWord;
   }
 }
---
+++ src/types/Utils/SetWordEnding.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+
+export enum Declination {
+  First = 1,
+  Second = 2,
+  Third = 3,
+}
+
+export enum WordGender {
+  Masculine = 1,
+  Feminine = 2,
+}
