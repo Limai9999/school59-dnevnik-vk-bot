dbb165b357eaa9755e29304f1495af9d54bce422
Limai <sasha.nafasov@gmail.com>
2022-10-22 16:58:21 +0500

    в личных сообщениях теперь не нужно шифровать пароль

--- src/commands/netcity.ts
+++ src/commands/netcity.ts
@@ -1,13 +1,27 @@
+import Password from '../modules/Password';
 import {CommandInputData, CommandOutputData} from '../types/Commands';
 
 async function command({message, args, vk, classes}: CommandInputData) {
-  const [login, password, className] = args;
+  const [login, inputPassword, className] = args;
 
-  if (login.length < 4 || password.length < 35) {
+  if (login.length < 4 || inputPassword.length < 4 || (inputPassword.length < 35 && !message.isDM)) {
+    let resultMessage = 'Введите корректные данные.\nЛогин и пароль должны быть больше 4 символов';
+
+    if (!message.isDM) {
+      resultMessage += ', причем пароль должен быть зашифрован командой "зашифровать пароль".';
+    }
+
     return vk.sendMessage({
-      message: 'Введите корректные данные.\nЛогин должен быть больше 4 символов, а пароль зашифрован командой "шифр".',
+      message: resultMessage,
       peerId: message.peerId,
       priority: 'low',
+    });
+  }
+
+  if (inputPassword.length >= 35 && message.isDM) {
+    return vk.sendMessage({
+      message: 'Для изменения данных Сетевого Города в личных сообщениях вам не нужно зашифровывать пароль.',
+      peerId: message.peerId,
     });
   }
 
@@ -19,6 +33,10 @@ async function command({message, args, vk, classes}: CommandInputData) {
       priority: 'low',
     });
   }
+
+  const password = message.isDM ?
+        new Password(inputPassword, false).encrypt() :
+        inputPassword;
 
   await classes.setNetCityData(message.peerId, {
     login,
