d8a9b9189e13823eb84a2ab4ee5a2d01277e794c
Limai <sasha.nafasov@gmail.com>
2022-10-22 16:43:13 +0500

    фикс аргументов

--- src/handlers/handleMessage.ts
+++ src/handlers/handleMessage.ts
@@ -112,9 +112,7 @@ export default async function handleMessage({message, classes, vk, vkUser, comma
   let args: string[] = [];
 
   if (text) {
-    args = text
-        .toLowerCase()
-        .replace(foundCommandAlias, '')
+    args = utils.caseInsensitiveReplace(text, foundCommandAlias, '')
         .trim()
         .split(' ')
         .filter((arg) => arg.length);
