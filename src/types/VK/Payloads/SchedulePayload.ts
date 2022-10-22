d67faa7fd7b8d008b1749c996a7ead400bad8da5
Limai <sasha.nafasov@gmail.com>
2022-10-22 16:43:56 +0500

    убран неиспользуемое действие

--- src/types/VK/Payloads/SchedulePayload.ts
+++ src/types/VK/Payloads/SchedulePayload.ts
@@ -3,7 +3,7 @@ import {Payload} from './Payload';
 export interface SchedulePayload extends Payload {
   command: 'schedule'
   data: {
-    action: 'get' | 'update' | 'choose' | 'netCityGetToday';
+    action: 'get' | 'update' | 'choose';
     filename?: string;
     type?: 'netcity' | 'manual'
   };
