7197dea81453d7f80ba530bb55b08ee3d4605b11
Limai <sasha.nafasov@gmail.com>
2023-05-09 00:02:13 +0500

    переделан показ списка команд

--- src/commands/commandsList.ts
+++ src/commands/commandsList.ts
@@ -2,23 +2,68 @@ import { Keyboard } from 'vk-io';
 import { CommandInputData, CommandOutputData } from '../types/Commands';
 
 async function command({ message, vk, commands }: CommandInputData) {
+  const peerId = message.peerId;
   const isAdminChat = message.peerId === vk.config.adminChatID;
+  const isDMChat = message.isDM;
 
-  let cmdCount = 0;
-  const commandsFormatted = commands.map(({ name, howToUse, description, requirements: { admin }, showInCommandsList }) => {
-    if (!isAdminChat && admin) return;
-    if (!showInCommandsList) return;
+  const showCommandsList = commands.filter((cmd) => cmd.showInCommandsList);
 
-    cmdCount++;
+  let meaningsMessage = '';
+  const addMeaningMessage = (msg: string) => meaningsMessage.length ? meaningsMessage += `\n${msg}` : meaningsMessage += msg;
+  const meaningsShowed = {
+    admin: false,
+    dmOnly: false,
+    paidSubscription: false,
+  };
 
-    return `${cmdCount}. ${name}${description ? `: ${description}` : ''}${howToUse ? `\nИспользование: ${name} ${howToUse}` : ''}`;
-  }).filter(Boolean);
+  let cmdIndex = 0;
+  const commandsFormatted = showCommandsList.map((cmd) => {
+    const { name, description, howToUse, requirements: { admin, dmOnly, paidSubscription, payloadOnly } } = cmd;
 
-  const result = `Всего команд - ${commandsFormatted.length}\nСписок доступных команд:\n\n${commandsFormatted.join('\n\n')}`;
+    if (dmOnly && !isDMChat) return;
+    if (admin && !isAdminChat) return;
 
-  vk.sendMessage({
-    message: result,
-    peerId: message.peerId,
+    let emoji = '';
+    if (admin || dmOnly || paidSubscription) emoji += ' ';
+    if (admin) {
+      if (!meaningsShowed.admin) {
+        addMeaningMessage('🛠️ - только для администраторов');
+        meaningsShowed.admin = true;
+      }
+
+      emoji += '🛠️';
+    }
+    if (dmOnly) {
+      if (!meaningsShowed.dmOnly) {
+        addMeaningMessage('📥 - только в личных сообщениях');
+        meaningsShowed.dmOnly = true;
+      }
+
+      emoji += '📥';
+    }
+    if (paidSubscription) {
+      if (!meaningsShowed.paidSubscription) {
+        addMeaningMessage('💸 - требуется активная подписка');
+        meaningsShowed.paidSubscription = true;
+      }
+
+      emoji += '💸';
+    }
+    if (admin || dmOnly || paidSubscription) emoji += ' ';
+
+    cmdIndex++;
+
+    return `${cmdIndex}. ${emoji}${name}\nОписание: ${description || 'нет'}\n${payloadOnly ? '⌨️ Работает только по кнопкам.' : howToUse ? `Использование: ${howToUse}` : ''}`;
+  });
+
+  const commandsFiltered = commandsFormatted.filter((cmd) => cmd);
+
+  const chatString = isDMChat ? 'Вам' : 'В этой беседе';
+  const finalMessage = `${chatString} доступно ${cmdIndex} команд:\n\n${commandsFiltered.join('\n\n')}\n\n${meaningsMessage}`;
+
+  await vk.sendMessage({
+    message: finalMessage,
+    peerId,
     priority: 'medium',
   });
 }
@@ -36,6 +81,7 @@ const cmd: CommandOutputData = {
     dmOnly: false,
     args: 0,
     paidSubscription: false,
+    payloadOnly: false,
   },
   keyboardData: {
     color: Keyboard.SECONDARY_COLOR,
